937598ce334505b2fcd72acacb09f7f8069010b6
jeesun <emoy.kim@gmail.com>
2023-09-28 18:34:24 +0900

    style: minor

--- source/camera.cpp
+++ source/camera.cpp
@@ -1,7 +1,7 @@
 #include "camera.h"
 
 CameraGL::CameraGL(float near_plane, float far_plane) :
-   Width( 0 ), Height( 0 ), NearPlane( near_plane ), FarPlane( far_plane ), ProjectionMatrix(glm::mat4(1.0f) )
+   Width( 0 ), Height( 0 ), NearPlane( near_plane ), FarPlane( far_plane ), ProjectionMatrix( glm::mat4(1.0f) )
 {
 }
 
@@ -10,11 +10,8 @@ void CameraGL::updateWindowSize(int width, int height)
    Width = width;
    Height = height;
    ProjectionMatrix = glm::ortho(
-      0.0f,
-      static_cast<float>(width),
-      0.0f,
-      static_cast<float>(height),
-      NearPlane,
-      FarPlane
+      0.0f, static_cast<float>(width),
+      0.0f, static_cast<float>(height),
+      NearPlane, FarPlane
    );
 }
